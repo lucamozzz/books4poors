4f63dfc37bd5d7e255b91c3f9e9e35f19af22c30
Luca Mozzoni <76916015+lucamozzz@users.noreply.github.com>
2022-05-09 19:01:00 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import FooterCSS from '../style/Footer.module.css';
 export const Footer = () => {
     return (
         <footer className={FooterCSS.container}>
-            <a href="https://github.com/motsiiih" target="_blank" rel="noreferrer">
+            <a href="https://github.com/lucamozzz" target="_blank" rel="noreferrer">
                 <FaGithub />
             </a>
             <p>Luca Mozzoni - 2021</p>
